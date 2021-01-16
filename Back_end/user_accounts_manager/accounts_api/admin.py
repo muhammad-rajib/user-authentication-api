5bd93b1e81b358d56b32680525696539c35555e7
rajibhossain8521 <rajibhossain8521@gmail.com>
2021-01-16 11:14:01 +0600

    Defined UserAdmin class

--- Back_end/user_accounts_manager/accounts_api/admin.py
+++ Back_end/user_accounts_manager/accounts_api/admin.py
@@ -1,5 +1,17 @@
+# from django.contrib import admin
+# from .models import registered_accounts
+
+# # Register your models here.
+# admin.site.register(registered_accounts)
+
 from django.contrib import admin
-from .models import registered_accounts
 
 # Register your models here.
-admin.site.register(registered_accounts)
+from .models import registered_accounts
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'created_at']
+
+
+admin.site.register(registered_accounts, UserAdmin)
