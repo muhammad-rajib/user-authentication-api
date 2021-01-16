5ea3267f1be8a13579d60cc49d1be23cdd5051de
rajibhossain8521 <rajibhossain8521@gmail.com>
2021-01-16 11:12:28 +0600

    Login, Logout path added

--- Back_end/user_accounts_manager/accounts_api/urls.py
+++ Back_end/user_accounts_manager/accounts_api/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path
 
-from .views import registration_view, VerifyEmail
+from .views import (
+    registration_view, 
+    VerifyEmail, 
+    LoginView, 
+    ResendVerifyEmail,
+    LogoutView,
+)
+
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 app_name = 'accounts_api'
 
 urlpatterns = [
     path('register/', registration_view, name='register'),
     path('verify-email/', VerifyEmail.as_view(), name='email-verfify'),
+    path('resend-verify-email/', ResendVerifyEmail, name='resend-verify-email'),
+    path('user-login/', LoginView.as_view(), name='user-login'),
+    path('user/logout/', LogoutView.as_view(), name='logout'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
